396cbec826c86886913e441378b1fcb12786a87d
Jobin Varghese <jobinvarghese7959@gmail.com>
2024-03-13 21:28:31 +0530

    ELMS

---
+++ ELMS/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import redirect,render
+from app.models import Categories,Course,Level
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+def BASE(request):
+    return render(request,'base.html')
+
+
+def HOME(request):
+    category=Categories.objects.all().order_by('id')[0:5]
+    course = Course.objects.filter(status = 'PUBLISH').order_by('-id')
+    context = {
+        'category':category,
+        'course':course,
+    }
+    return render(request,'Main/home.html',context)
+
+
+def SINGLE_COURSE(request):
+    category = Categories.get_all_category(Categories)
+    level = Level.objects.all()
+    course = Course.objects.all()
+    context = {
+        'category':category,
+        'level': level,
+        'course':course
+    }
+    return render(request,'Main/single_course.html',context)
+
+def filter_data(request):
+    category = request.GET.getlist('category[]')
+    level = request.GET.getlist('level[]')
+    price = request.GET.getlist('price[]')
+
+    if price == ['PriceFree']:
+        course = Course.objects.filter(price=0)
+    elif price == ['PricePaid']:
+        course = Course.objects.filter(price__gte=1)
+    elif price == ['PriceAll']:
+        course = Course.objects.all()
+    elif category:
+        course = Course.objects.filter(category__id__in = category).order_by('-id')
+    elif level:
+        course = Course.objects.filter(level__id__in = level).order_by('-id')
+    else:
+        course = Course.objects.all().order_by('-id')
+    t = render_to_string('ajax/course.html', {'course': course})
+    return JsonResponse({'data': t})
+
+
+
+def CONTACT_US(request):
+    return render(request,'Main/contact_us.html')
+
+
+def ABOUT_US(request):
+    return render(request,'Main/about_us.html')
